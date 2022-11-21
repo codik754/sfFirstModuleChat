b90e5b022052a8658bc008ec46911d837968000f
codik <5ochta@bk.ru>
2022-11-21 19:36:46 +0700

    Added classes Message and Datetime. Maked setter and getter for classes.

---
+++ datetime.cpp
@@ -0,0 +1,70 @@
+﻿//Файл datetime.cpp
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "datetime.h"
+#include <ctime>
+#include <sstream>
+
+//Конструктор по умолчанию
+Datetime::Datetime(){
+    std::time_t t = std::time(nullptr);
+    std::tm *now = localtime(&t);
+
+    sec_ = now->tm_sec;
+    min_ = now->tm_min;
+    hour_ = now->tm_hour;
+    day_ = now->tm_mday;
+    month_ = now->tm_mon + 1;
+    year_ = now->tm_year + 1900;
+}
+
+
+//Установить текущие время
+void Datetime::setNow(){
+    std::time_t t = std::time(nullptr);
+    std::tm* now = localtime(&t);
+
+    sec_ = now->tm_sec;
+    min_ = now->tm_min;
+    hour_ = now->tm_hour;
+    day_ = now->tm_mday;
+    month_ = now->tm_mon + 1;
+    year_ = now->tm_year + 1900;
+}
+
+//Получить секунды
+int Datetime::getSec() const {
+    return sec_;
+}
+
+//Получить минуты
+int Datetime::getMin() const {
+    return min_;
+}
+
+//Получить часы
+int Datetime::getHour() const {
+    return hour_;
+}
+
+//Получить день
+int Datetime::getDay() const {
+    return day_;
+}
+
+//Получить месяц
+int Datetime::getMonth() const {
+    return month_;
+}
+
+//Получить год
+int Datetime::getYear() const {
+    return year_;
+}
+
+//Получить дату и время в виде строки
+const string Datetime::getStr() const {
+    std::ostringstream os;
+    os << day_ << "." << month_ << "." << year_ << "  " << hour_ << ":" << min_ << ":" << sec_;
+    return os.str();
+}
---
+++ datetime.h
@@ -0,0 +1,38 @@
+﻿//Файл datetime.h
+#pragma once
+
+#include <string>
+
+using std::string;
+
+class Datetime
+{	
+	int sec_;  //секунды
+	int min_;  //минуты
+	int hour_; //час
+	int day_;  //день
+	int month_;//месяц
+	int year_; //год
+public:
+	//Конструктор по умолчанию
+	Datetime();
+
+	//Установить текущие время
+	void setNow();
+
+	//Получить секунды
+	int getSec() const;
+	//Получить минуты
+	int getMin() const;
+	//Получить часы
+	int getHour() const;
+	//Получить день
+	int getDay() const;
+	//Получить месяц
+	int getMonth() const;
+	//Получить год
+	int getYear() const;
+	//Получить дату и время в виде строки
+	const string getStr() const;
+};
+
